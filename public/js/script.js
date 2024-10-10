e258e457b4a4fdfc65fa8526f693e7384786a0f1
Suphawit Hngosakul - 6609650053 <145352874+Suphawit-Hngosakul@users.noreply.github.com>
2024-10-11 00:43:13 +0700

    initial node.js web project

--- public/js/script.js
+++ public/js/script.js
@@ -23,13 +23,15 @@ function call_REST_API_Hello() {
     const password = document.getElementById('password').value;
 
     const url = (
-        'http://localhost:8080/hello?' +
+        'http://localhost:8080/services/hello?' +
         new URLSearchParams({ myName: username, lastName: password}).toString()
       );
     
     fetch(url)
-    .then(data => {
-        document.getElementById('message').innerText = data.message;
+    .then(response => response.text())
+    .then(text => {
+        document.log("Text return from REST API: "+text);
+        document.getElementById('message').innerText = text;
     })
     .catch(error => console.error('Error:', error));
 }
