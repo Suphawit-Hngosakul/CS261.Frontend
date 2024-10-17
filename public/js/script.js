fe974d0b6e8ca8dfee8c5bc312c20e6ef0db8154
Suphawit Hngosakul - 6609650053 <145352874+Suphawit-Hngosakul@users.noreply.github.com>
2024-10-17 23:27:50 +0700

    add  show_password

--- public/js/script.js
+++ public/js/script.js
@@ -118,6 +118,17 @@ function checkInputs() {
     }
 }
 
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+
+    // เปลี่ยนข้อความและไอคอน
+    this.textContent = type === 'password' ? ' Show' : ' Hide';
+    this.classList.toggle('show-password');
+});
+
+
 document.getElementById('username').addEventListener('input', checkInputs);
 document.getElementById('password').addEventListener('input', checkInputs);
 document.getElementById('role').addEventListener('change', checkInputs);
